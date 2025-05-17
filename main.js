a081cf2f192d7fa9718075444a0f1344ba3abef6
Renaldo Hyacinthe <renaldohyacinthe@nyu.edu>
2025-05-17 19:30:22 -0400

    fini

---
+++ main.js
@@ -0,0 +1,35 @@
+const body = document.body;
+const validOrder = ["section", "article", "div", "b"];
+let answerString = "";
+
+function validTreeStructure(node, level){
+    
+    const validElement = node.tagName.toLowerCase() === validOrder[level];
+    
+    const needsClassCheck = level != 3 || 
+    level == 3 && 
+    !node.className.includes("ref0") &&
+     node.className.includes("ref")  ;
+
+    if (!validElement) return;
+    //console.log("HI")
+    if (level==3 && needsClassCheck) {
+        answerString = answerString + node.getAttribute("value");
+        return; 
+        
+        //implement addition to answer
+    }
+    level++;
+    for (const ele of node.childNodes){
+        validTreeStructure(ele, level);
+    }
+
+return;
+}
+
+for (const element of document.body.children){
+    validTreeStructure(element, 0);
+}
+
+console.log(answerString);
+
